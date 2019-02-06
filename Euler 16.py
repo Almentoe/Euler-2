9e51f0b10d59182558f40578950f82b11c6c0157
Almentoe <32596024+Almentoe@users.noreply.github.com>
2019-02-06 14:51:10 +0000

    Delete

--- Euler 16.py
+++
@@ -1,9 +0,0 @@
-A = 2**1000
-print(str(A))
-B = str(A)
-Sum = 0
-i = 0
-while i < len(B):
-    Sum = Sum + int(B[i])
-    i = i + 1
-print(Sum)    
--- Euler 9.py
+++
@@ -1,8 +0,0 @@
-N = 1000
-for c in range(1,N):
-    for b in range(1,N):
-        for a in range(1,N):
-            if a + b + c == 1000 and a**2 + b**2 == c**2:
-                print(a*b*c)
-                
-# This is Very Very Slow                
